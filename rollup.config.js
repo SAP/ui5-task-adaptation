8520d03057af6e777d42ac2b7643e7410c48affe
Aleksandr Suvorov <aleksandr.suvorov@sap.com>
2021-02-04 18:05:42 +0100

    Use internal project settings for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,35 @@
+//@ts-check
 const path = require("path");
-const ConfigBuilder = require("./rollup/configBuilder");
-module.exports = ConfigBuilder.build(path.resolve(__dirname, "..", "..", ".."));
+const ui5 = require("./rollup/ui5-resolve");
+const builtins = require('builtin-modules');
+const { terser } = require("rollup-plugin-terser");
+const { nodeResolve } = require("@rollup/plugin-node-resolve");
+
+module.exports = {
+    input: "./rollup/bundle-def.js",
+    plugins: [
+        ui5({
+            assets: [
+                "/resources/sap/ui/fl/**"
+            ],
+            skipTransformation: [
+                "./rollup/bundle-def.js",
+                "sap/ui/thirdparty/URI"
+            ],
+            projectPath: "./rollup/project"
+        }),
+        nodeResolve({
+            preferBuiltins: true
+        }),
+        terser({
+            numWorkers: 1
+        })
+    ],
+    external: builtins,
+    output: [
+        {
+            file: "./dist/bundle.js",
+            format: "commonjs"
+        }
+    ]
+};
--- test/rollup.config.js
+++
@@ -1,1 +0,0 @@
-module.exports = require("../rollup/configBuilder").build(`${__dirname}/resources/project`);
