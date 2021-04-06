38122b80d581dd296dbcb6d88168d58b43c3ca91
Aleksandr Suvorov <aleksandr.suvorov@sap.com>
2021-04-06 18:24:30 +0200

    Use internal ui5 API

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 //@ts-check
 const path = require("path");
 const ui5 = require("./rollup/ui5-resolve");
-const updateUI5Version = require("./rollup/ui5-version");
 const builtins = require("builtin-modules");
 const { nodeResolve } = require("@rollup/plugin-node-resolve");
 
@@ -10,10 +9,6 @@ const FALLBACK_PROJECT_FOLDER = path.resolve(__dirname, "rollup", "project");
 module.exports = {
     input: "./rollup/bundle-def.js",
     plugins: [
-        updateUI5Version({
-            uri: "https://openui5.hana.ondemand.com/neo-app.json",
-            projectPath: path.join(FALLBACK_PROJECT_FOLDER, "ui5.yaml")
-        }),
         ui5({
             assets: [
                 "/resources/sap/ui/fl/**"
