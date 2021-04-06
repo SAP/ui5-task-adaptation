b08bc4c7ba61039a1b144346ab3796e413649650
Aleksandr Suvorov <aleksandr.suvorov@sap.com>
2021-04-06 18:26:18 +0200

    Polish

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,6 @@ const ui5 = require("./rollup/ui5-resolve");
 const builtins = require("builtin-modules");
 const { nodeResolve } = require("@rollup/plugin-node-resolve");
 
-const FALLBACK_PROJECT_FOLDER = path.resolve(__dirname, "rollup", "project");
-
 module.exports = {
     input: "./rollup/bundle-def.js",
     plugins: [
@@ -19,7 +17,7 @@ module.exports = {
             ],
             projectPaths: [
                 path.resolve(__dirname, "..", "..", ".."),
-                FALLBACK_PROJECT_FOLDER
+                path.resolve(__dirname, "rollup", "project")
             ]
         }),
         nodeResolve({
